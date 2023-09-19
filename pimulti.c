bd8a1ef4efc850fc6e276c32210a5ddf0dbcad04
Danieljct <daniel.cubillost@usm.cl>
2023-09-19 18:24:47 -0400

    multi

---
+++ pimulti.c
@@ -0,0 +1,32 @@
+//gcc -fopenmp -lm  main.c -o main
+
+#include <math.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+float total, total1 = 0, puntosdentro;
+
+int main(int argc, char *argv[]) {
+
+total = atoi(argv[1]);
+  
+#pragma omp parallel
+  {
+    srand(time(NULL));
+    for (int i = 0; i < total; i++) {
+      double numero1 = ((double)rand() / RAND_MAX) * 2.0 - 1.0;
+      double numero2 = ((double)rand() / RAND_MAX) * 2.0 - 1.0;
+      if (pow(pow(numero1, 2) + pow(numero2, 2), .5) <= 1)
+        puntosdentro++;
+      total1++;
+    }
+  }
+
+  double pi = 4 * puntosdentro / total1;
+  printf("PI: %f\n", pi);
+
+  return 0;
+}
+
